65317a3010dc701a7a4989df52695dcad2af83ba
ian <ian@batrac.com>
2018-04-22 14:41:49 +0700

    mynotes app

---
+++ app/src/main/java/com/dicoding/associate/mynotesapp/db/DatabaseContract.java
@@ -0,0 +1,17 @@
+package com.dicoding.associate.mynotesapp.db;
+
+import android.provider.BaseColumns;
+
+public class DatabaseContract {
+
+    static String TABLE_NOTE = "note";
+
+    static final class NoteColumns implements BaseColumns {
+        //Note title
+        static String TITLE = "title";
+        //Note description
+        static String DESCRIPTION = "description";
+        //Note date
+        static String DATE = "date";
+    }
+}
